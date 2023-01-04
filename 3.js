3639dc4af7f01731d6a63d037a4341210be68e11
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-01-04 18:24:52 +0530

    version 2.0

---
+++ 3.js
@@ -0,0 +1,15 @@
+alert("hello world")
+
+var a=12;
+var b=56;
+console.log(a+b)
+console.log(a-b)
+console.log(a/b)
+console.log(a%b)
+console.log(a&&b)
+console.log(a||b)
+console.log(a*b)
+console.log(a**b)
+
+
+
